a1372d2f7e0a8f1ac6e77871ec87fece33e069aa
butterfly0204 <favourmogaji3@gmail.com>
2025-10-07 01:13:44 -0500

    i learnet about funtion

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-let bonusPoint = 50;
-console.log(bonusPoint)
-  bonusPoint = bonusPoint + 50
-  console.log(bonusPoint)
- bonusPoint = bonusPoint - 25
- console.log(bonusPoint)
-bonusPoint = bonusPoint + 70
- console.log(bonusPoint)
+function myNum(){
+  console.log(42);
+}
+myNum();
