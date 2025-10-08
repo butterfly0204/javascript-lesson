82f16b408a1bc69ef7477f35a2e97e3037dcd644
butterfly0204 <favourmogaji3@gmail.com>
2025-10-07 22:36:32 -0500

    counter Program

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
- let welcomeEl = document.getElementById("welcome-el")
-let name = "favour"
-let geeting = "Welcome back"
+let People  = document.getElementById("People-entered")
+let saveEl = document.getElementById("save-el")
+let count = 0
+function increment(){
+ count += 1
+   People.textContent = count
+}  
+increment()
 
-welcomeEl.innerText = geeting +" " +name
 
-welcomeEl.innerText += "👋"
+function save() {
+   let countStr = count + " - "
+   saveEl.textContent += countStr
+    count = 0
+
+ 
+    People.textContent = 0
+     
+}   
+
+save()
+
