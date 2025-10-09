b71638e0126bf24c9366ef25e9aefe7f4eeb18ae
butterfly0204 <favourmogaji3@gmail.com>
2025-10-09 08:12:56 -0500

    building  a black jack game

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-let num1 =5
-let num2 =2
+let firstCard =5
+let secondCard =8
 
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-let sum = document.getElementById("sum-el") 
+let sum = firstCard + secondCard
 
-
-
-function add(){
-  let result = (num1 + num2)
-
-  sum.textContent= "sum:" + result
-  
-
-
-}
-function substact(){
- let result = (num1 - num2)
-
-  sum.textContent= "sum:" + result
-  
-
-}
-function divide(){
-
-let result = (num1 / num2)
-
-  sum.textContent= "sum:" + result
-  
-
-}
-function multiply(){
-let result = (num1 * num2)
-
-  sum.textContent= "sum:" + result
-  
-
-}
-
- 
-
+console.log(sum) 
