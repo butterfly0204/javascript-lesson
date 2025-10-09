40a125b2d3dfc10e6e0e89198e3f48c11ace7ef6
butterfly0204 <favourmogaji3@gmail.com>
2025-10-09 13:04:57 -0500

    building my project

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
 
 
@@ -22,9 +23,9 @@ else {
  
   isAlive = false
 }
-console.log(message)
+messageEl.textContent = message
 
-console.log(isAlive)
+
 
 
 }
