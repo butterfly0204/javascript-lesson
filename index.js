b11e270ca6e15e6c274a8311143ced932d120438
butterfly0204 <favourmogaji3@gmail.com>
2025-10-09 08:43:04 -0500

    more if/else condition practice

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let age = 20
-
-if (age < 21) {
-    console.log("You cannot enter the club")
-
-  
-  }
-  else{
-    console.log("Welcome!")
-  }
+let age = 90
+if (age < 100){
+    console.log("You are not eligible")
+}
+else if (age <= 100){
+  console.log("Here is your bithday card from the king")
+}
+else {
+  console.log("Not eligible, You hve already gotton one")
+}
