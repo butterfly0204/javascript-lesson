2562b480bbae7936baaac48f9de2c967aa1f860f
butterfly0204 <favourmogaji3@gmail.com>
2025-10-10 07:30:49 -0500

    learnig the for loop

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
- let firstCard = 12
-let secondCard = 9
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl= document.getElementById("cards-el") 
+for(i =10; i<101; i+=10)
 
-
-
-function startGame() {
-  if (sum <= 20){
-  message = "Do you want to draw a new card?"
-  sumEl.textContent = "Sum: " + sum
-
-}
-else if (sum === 21){
-  message = "Wohoo! You've got Blackjack!"
-    sumEl.textContent = "Sum: " + sum
-
-  hasBlackJack = true
-}
-else {
-  message = "You're out of the game!"
- 
-  isAlive = false
-    sumEl.textContent = "Sum: " + sum
-}
-
-
-messageEl.textContent = message
-cardsEl.textContent ="Cards:" + firstCard + "   " + secondCard
-
-
-}
-
+  console.log(i);
 
