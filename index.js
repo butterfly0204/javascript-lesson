647b1881b13250d92c265b57c851349b2a68bdd8
mogaji favour <favourmogaji3@gmail.com>
2025-10-11 13:59:20 -0500

    mor of dice prcical

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let randomNumber = Math.floor(Math.random() * 6) +1
 
-console.log(randomNumber)
+function rollDice() {
+
+ let random = Math.floor(Math.random() * 6) +1
+ return random
+}
+
+ console.log(rollDice())
