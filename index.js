1102886701168b7fb615d89aac0c82d38a6a4d88
mogaji favour <favourmogaji3@gmail.com>
2025-10-14 09:33:28 -0500

    javascript challenges

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-let largestCountries = ["Tuvalu", "india", "usa,", "indonesia", "monaco"]
+let emojis = ["🦁", "🦍", "🐕‍🦺", "🐘", "🐅", "🐐"]
 
-largestCountries.pop()
-largestCountries.push("china", "pakistan")
-console.log(largestCountries)
+function fighter(){
+  let pickFighters =Math.floor(Math.random() *6)
+  return emojis[pickFighters] 
+}
+
+console.log(fighter())
+console.log(fighter())
