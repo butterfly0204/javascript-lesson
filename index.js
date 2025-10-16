7736c5203951779d2ddcb28a644b1a137f44bc56
mogaji favour <favourmogaji3@gmail.com>
2025-10-16 09:27:31 -0500

    good morning...

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let myLeads= ['']
-const inputEL = document.getElementById("input-el")
 
+let myLeads = ["wwwmawesomelead.com", "www.epiclead.com", "www.geatlead,com"]
+let inputEL = document.getElementById("input-el")
 
 const inputBtn = document.getElementById("input-btn")
 
 
 
 inputBtn.addEventListener("click", function(){
-    myLeads.push("www.awesomeload.com")
+    myLeads.push(inputEL.value)
     console.log(myLeads)
-} )  
+} )     
+
+for (let i=0; i < myLeads.length; i++){
+    console.log(myLeads[i])
+
+
+}
+
