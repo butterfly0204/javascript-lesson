5ff2f70131017447fd612bb3120ed825f7cf59da
mogaji favour <favourmogaji3@gmail.com>
2025-10-18 10:07:10 -0500

    hello guys

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 
-let myLeads = ["wwwmawesomelead.com", "www.epiclead.com", "www.geatlead,com"]
-let inputEL = document.getElementById("input-el")
+let myLeads = []
+const inputEL = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-
 const inputBtn = document.getElementById("input-btn")
 
 
 
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
-    console.log(myLeads)
-} ) 
+    renderLeads()
+}) 
 
-
-for (let i=0; i < myLeads.length; i++){
-  // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-
-
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++){
+   listItems += "<li>" + myLeads[i] + "</li>"
+  }
+  ulEl.innerHTML = listItems
 }
 
-const li =document.createElement("li")
- li.textContent =myLeads[i]
-ulEl.append(li)
+
