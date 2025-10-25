3e141b991805313180582c150c54a8fb6638caee
mogaji favour <favourmogaji3@gmail.com>
2025-10-25 13:50:39 -0500

    hello, its me again

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-generateSentence(des, arr){
-  let baseString = `the ${arr.length} ${des}  are `
-  for(i=0; i < arr.length; i++){
-    baseString += arr[i] + ","
+ const sentence = generateSentence("countries", ["america", "nigeria", "london"])
+ 
+ function generateSentence(desc, arr){
 
-  }
+    for (i=0;  i< arr.length; i++){
+        let baseString =`the ${arr.length} ${desc} are`
+        baseString += arr[i]
+    }
 
-
-}
+ }
