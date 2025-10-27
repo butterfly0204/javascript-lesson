6d5ef73ddd2ed3f2a04bb18798d8368503ba2339
mogaji favour <favourmogaji3@gmail.com>
2025-10-27 09:43:42 -0500

    rounding and strind conversion

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-  const totalPrice = Math.round(420.69235632455)
+  const totalPrice = Number("420.69235632455")
   const btn = document.getElementById("purchase-btn")
 
 
-  btn.textContent = `buy $${ totalPrice}`
+  btn.textContent = `buy $${ totalPrice.toFixed(2)}`
