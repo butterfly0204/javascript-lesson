6da95b5fa698d235894500654b994221776197c3
favour mogaji <favourmogaji3@gmail.com>
2025-10-30 14:34:21 -0500

    chaangesin operator

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const playerGuss = 6
-const correctAnswer = 6
+const playerGuss = 5
+const correctAnswer = 5
 
 
-let message = playerGuss === correctAnswer? "Correct" : 
-playerGuss > correctAnswer? " too high" : playerGuss < correctAnswer? "too low" :"wrong"
+let message = 
+playerGuss > correctAnswer ? " too high" : playerGuss < correctAnswer? "too low" :"Correct" 
 
 console.log (message)
