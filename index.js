ba4443806a1ab75bc04b1407169cf2531cbe9a34
favour mogaji <favourmogaji3@gmail.com>
2025-10-30 14:31:48 -0500

    more ternary operator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const playerGuss = 6
 const correctAnswer = 6
 
 
-let message = playerGuss === correctAnswer? "Correct" : "wrong"
+let message = playerGuss === correctAnswer? "Correct" : 
+playerGuss > correctAnswer? " too high" : playerGuss < correctAnswer? "too low" :"wrong"
 
 console.log (message)
