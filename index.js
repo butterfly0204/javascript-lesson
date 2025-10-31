c6bdcef9144c8724a2158c032b7276212809dc2f
favour mogaji <favourmogaji3@gmail.com>
2025-10-31 10:39:05 -0500

    cases and swithes

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ function selectItem(item) {
 
     switch(item){
         case 'coffee':
-        price = 2
-         break
+             price = 2
+             break
+
+         case 'lemon cake':
+             price = 3
+             break
+
+        case 'salad':
+             price = 4
+             break
+        case 'sandwiches':
+             price = 5
+             break
+        
     
         }
         return `you selected ${item}, Tha will be $${price}`
 
+       
+
+
 }
 
 console.log(selectItem('coffee'))
+console.log(selectItem('salad'))
+console.log(selectItem('lemon cake'))
+console.log(selectItem('sandwiches'))
