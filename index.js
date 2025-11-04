b3aa5c14c8935c01e1c20ecd67dbd2ddbe790f9e
favour mogaji <favourmogaji3@gmail.com>
2025-11-04 08:52:41 -0600

    date constructor

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// settimeout with parameters//
-function logAnswer(name, points) {
-  console.log(`THe answer is ${name} of course! If you got that right, give yourself ${points} Points.`)
-}
-setTimeout(logAnswer, 3000, 'lima', '10')
+// the date() constructor
 
+ const dateSnaSHot = new Date();
 
-console.log(`What is th capital of Peru?`)
+ console.log (typeof dateSnaSHot)
