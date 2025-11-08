b5d546f687792862f3e24440e188c9d01512b907
favour mogaji <favourmogaji3@gmail.com>
2025-11-08 12:14:13 -0600

    stock ticker challenge

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// the error() constructor
-function checkUsername(userName) {
-    console.log(userName)
+// super challenge stock ticker
+
+function getStockData(){
+    return {
+        name: `QtechAI`,
+        Sym: `QTA`,
+        price: (Math.random() *3).toFixed(2),
+        time:
+    }
 }
-checkUsername(`mogajiFavour`)
