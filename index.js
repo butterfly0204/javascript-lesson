ddfbea781525d7dec4919ef5f8a78c6d69957ce5
favour mogaji <favourmogaji3@gmail.com>
2025-11-08 19:42:50 -0600

    solving the challenge

--- index.js
+++ index.js
@@ -2,24 +2,30 @@
 import { getStockData } from './fakeStockAPI.js';
 
 
-setInterval( function(){ 
-    const stockData = getStockData();
-    renderStockTicker(stockData);
+setInterval( function() { 
+    const stockData = getStockData()
+    renderStockTicker(stockData)
 
 }, 1500)
-function renderStockTicker(){
+
+
+function renderStockTicker(stockData){
     const stockDisplayName = document.getElementById('name');
     const stockDisplaySymbol = document.getElementById('symbol');
     const stockDisplayPrice = document.getElementById('price');
     const stockDisplayPriceIcon = document.getElementById('price-icon');
     const stockDisplayTime = document.getElementById('time'); 
-}
 
-const {name, Sym, price, time} = getStockData();
+
+    const {name, Sym, price, time} = stockData
+
 stockDisplayName.innerText = name;
 stockDisplaySymbol.innerText = Sym;
 stockDisplayPrice.innerText = `$${price}`;
 stockDisplayTime.innerText = time;
+}
+
+
 
 
 
