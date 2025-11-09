e7d81c6db93fa301483f05bafa2895f41ece141f
favour mogaji <favourmogaji3@gmail.com>
2025-11-09 11:39:38 -0600

    solving challanges

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //Arrow funtions
-const speedWarning = (speed) => {
-    return `you are going at ${speed} mph!`;
+const speedWarning = (speed,driverSpeed) => {
+    return ` speed limit is ${speed}you are going at ${driverSpeed} mph!`;
 }
 
-console.log (speedWarning(40));
+console.log (speedWarning(40, 80));
 
 
 
