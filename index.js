cb6edea7d42eed536abc097544f18ee27c30af57
favour mogaji <favourmogaji3@gmail.com>
2025-11-13 12:10:44 -0600

    rest parameter challenge

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//inline Arrow funtions
+//rest paramter example 
 
 //  Extra: Didnt commit for two days and i am already forgeting i have been doing, Nevr go a day withot coding  guys//
-function setPermissionLevel(permissionLevel, name1, name2, name3) {
-    console.log(`${name1} has ${permissionLevel} level access.`)
-    console.log(`${name2} has ${permissionLevel} level access.`)
-    console.log(`${name3} has ${permissionLevel} level access.`)
+function setPermissionLevel(permissionLevel, ...names) {
+names.forEach((name) => console.log (`${name} has been granted ${permissionLevel} access.`));
+   
     }
 
-    setPermissionLoveLevel("Admin", "Alice", "Bob", "Charlie");
+    setPermissionLevel("Admin", "Alice", "Bob", "Charlie");
 
