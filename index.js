751831251b0469a15d0632149e88d422cde73368
favour mogaji <favourmogaji3@gmail.com>
2025-11-12 21:52:38 -0600

    correction made

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const distanceTraveledMiles = [267, 345, 234, 190,299]
 
-const distanceTraveledKm = distanceTraveledMiles.map(function(distace){
-    return Math.round(distace * 1.6)
+const distanceTraveledKm = distanceTraveledMiles.map(function(distance){
+    return Math.round(distance * 1.6)
 })
  console.log(distanceTraveledKm)
 
