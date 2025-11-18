377afe77a7523be644aff17b49971949d7c97da6
favour mogaji <favourmogaji3@gmail.com>
2025-11-17 18:29:16 -0600

    introduction to JSON

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-//callback in javascript 
+//WHat is API? An Api is any tools that helps connect our program with someoneelse's program.
+//For example: when we use google maps in our app, we are using google maps API to connect our app with google maps program.
 
-function  notifyUser(notificationFn){
-    notificationFn()
-}
-
-const emailNotification = ()=> {
-    console.log("You have a new email")
-}
-const smsNotification = ()=> {
-    console.log("You have a new SMS")
-}
+//API stands for Application Programming Interface. It is a set of rules and protocols for building and interacting with software applications. APIs allow different software systems to communicate with each other, enabling them to share data and functionality.
 
 
-notifyUser(emailNotification)
-notifyUser(smsNotification)
+  
