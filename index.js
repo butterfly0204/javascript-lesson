77ceb8f7ead9ff90c65e64388164da7b562675b9
favour mogaji <favourmogaji3@gmail.com>
2025-11-21 09:04:32 -0600

    fetch and then API

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 //For example: when we use google maps in our app, we are using google maps API to connect our app with google maps program.
 
 //API stands for Application Programming Interface. It is a set of rules and protocols for building and interacting with software applications. APIs allow different software systems to communicate with each other, enabling them to share data and functionality.
-
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(response => response.json())
+.then(data => {
+ 
+    const imgElement = document.createElement('img')
+    imgElement.src = data.message
+    imgElement.alt = 'A Random Dog'
+    document.getElementById('img-container').appendChild(imgElement)
+   
+})
 
   
