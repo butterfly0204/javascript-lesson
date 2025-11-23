777ccc79459de8636341852af3c8034af88e87ef
favour mogaji <favourmogaji3@gmail.com>
2025-11-23 07:48:17 -0600

    usin try, catch and inally

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // Handling Rejected Promises
-        fetch('https://api.example.com/data')
-        .then(response => response.json())
-        .then(data => console.log(data))
-
-
- 
-
- 
+      
     try{
+         const response = await fetch('https://api.example.com/data')
+          const data = await  response.json()
+          console.log(data)
 
     } 
     catch(error){
