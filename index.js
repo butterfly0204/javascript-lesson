f79702c50f5c12fba1e3e70bc1d65c055dda5a52
favour mogaji <favourmogaji3@gmail.com>
2025-11-24 08:37:20 -0600

    pomise constuctor

--- index.js
+++ index.js
@@ -1,44 +1,8 @@
-// Advance API version
-// GET getting data 
-// POST posting data
-// PUT updating data
-// DELETE deleting data
-// PATCH updating partial data
+// The promise constructor
 
-
-try{
-const baseUrl = await fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
-    method: 'POST',
-     body: JSON.stringify( {
-        title: ' Holiday Nightmare',
-        body: 'My holiday was ruined by bad weather.',
-        userId: 100
-
-
-    }),
-    header:{
-    'content-Type':'application/json'
-}
+const myPromise = new Promise((resolve, reject) =>{
+    const success = Math.random() > 0.5
+    
 })
-if(!baseUrl.ok){
-    throw new Error(`HTTP error! status: ${baseUrl.status}`);
-}
-
-
-const getPosts = await baseUrl.json() 
-console.log(getPosts);
-}
-
-
-
-catch(error) {
-    console.error(error);
-}
-
-finally {
-    console.log("Fetch attempt finished.");
-}
-
-
 
  
