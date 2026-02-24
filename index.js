0fc10188aa9db29ef1d015d10917e01ce1a574f7
favour mogaji <favourmogaji3@gmail.com>
2026-02-23 18:05:26 -0600

    lets start

---
+++ index.js
@@ -0,0 +1,8 @@
+//git is a version control systemns 
+// runs locally on your machine 
+// Helps keeps history of changes you've made o your project 
+// WHILE    Githubis an online platform that stores git rpositories
+//  Run ONline on the cloud 
+// Helps Chre your code ( andthe history of changes ) with others
+// version control creates " savepoints (called commits) " in your code that you can revert back to if needed
+//manages changes  made by others  by merging multiple different versions of the same codebase
