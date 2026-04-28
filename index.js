2f358ee49c75351d6d8a0c741addfb1c61bc90aa
favour mogaji <favourmogaji3@gmail.com>
2026-04-28 12:40:19 -0500

    learning about functions

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
- /**let firstCard = 22
-let secondCard = 11
-
+ let firstCard = 11
+let secondCard =  1
 let sum = firstCard + secondCard
+let hasBlackJack = false
+isALive = true
+let message =  " "
+let sumEL = document.getElementById("sum-el");
+let messageEl = document.getElementById("mesage-el");
+let cardEl = document.getElementById("card-el");
 
 
+function startGame(){
 
-if(sum < 21){
+         sumEL.textContent = "sum: " + sum
+         cardEl.textContent = "Cards:" + firstCard + "   "  + secondCard
+    if(sum <= 20){
+    
+        message ="Do you want to draw a new card ? " 
+       
+    
+    }
+    else if(sum === 21){
+        message = "YOu've got Blackjack "
+        hasBlackJack = true
+    
+    }
+    else{
+    
+       message ="You are out of the game! "
+        isAlive = false
+    }
 
-    console.log("Do you want to draw a new card")
+    messageEl.textContent = message  
 
 }
-else if(sum === 21){
-    console.log("You win")
-
-}
-else{
-
-    console.log("Try again!")
-}
-    **/
-let age = 200
-
-if (age <100){
-    console.log("not eligible")
 
 
-}
 
-else if (age === 100){
-    console.log("Here is your birthday card from the kings")
-}
-else{
-    console.log("NOt eligible, YOu have already gotten one")
-}
+
+    
+
+
+
+    
